41d56fd340bac9d2f1bdbc45946c67c5f4f07941
Shanyou Li <shanyouli6@gmail.com>
2024-04-10 21:01:20 +0800

    add term_fmt_by dict

--- sd/api/macbid.py
+++ sd/api/macbid.py
@@ -5,7 +5,7 @@ from typing import List, Union
 import typer
 from sd.utils.cmd import cmd_getout
 from sd.utils.enums import Colors
-from sd.utils.fmt import term_fmt_by_list
+from sd.utils.fmt import term_fmt_by_dict, term_fmt_by_list
 
 app = typer.Typer()
 
@@ -18,12 +18,14 @@ def get_app_list_by_path(path: PathLink) -> List[str]:
     app_list = []
     for i in os.listdir(path):
         if i.endswith(".app"):
-            i_path = Path(i)
-            app_list.append(i_path.resolve() if i_path.is_symlink() else i)
+            i_path = Path(os.path.join(path, i))
+            app_list.append(
+                i_path.resolve().name if i_path.is_symlink() else i_path.name
+            )
         else:
-            subpath = f"{path}/{i}"
+            subpath = os.path.join(path, i)
             if os.path.isdir(subpath):
-                for j in get_app_list_by_path(os.path.join(path, i)):
+                for j in get_app_list_by_path(subpath):
                     app_list.append(j)
     return list(set(app_list))
 
@@ -38,66 +40,17 @@ def get_app_bundleid(app_name: str) -> str:
     return cmd_getout(f"osascript -e 'id of app \"{app_name}\"'")
 
 
-def get_terminal_columns() -> int:
-    columns = os.get_terminal_size().columns
-    if columns == 0:
-        columns = 80
-
-    return columns
-
-
-def get_list_max_size(lst: List[str]) -> int:
-    return max(len(i) for i in lst)
-
-
 @app.command(help="Get All App Name")
 def display():
     app_lists = get_app_list_by_list(app_path)
     term_fmt_by_list(app_lists)
-    # pre_size = len(str(len(app_lists)))
-    # columns = get_terminal_columns()
-    # app_size = get_list_max_size(app_lists)
-    # num = columns // (app_size + 4 + pre_size + 2)
-    # if num == 0:
-    #     num = 1
-    # messages = ""
-    # split = "".rjust(4)
-    # for i, appname in enumerate(app_lists):
-    #     numstr = typer.style(f"{i + 1}".rjust(pre_size, "0"), fg=Colors.INFO.value)
-    #     appname = typer.style(appname.ljust(app_size), fg=Colors.INFO.value)
-    #     message = f"{numstr}: {appname}{split}"
-    #     messages = f"{messages}{message}"
-    #     if (i + 1) % num == 0:
-    #         typer.echo(messages)
-    #         messages = ""
 
 
 @app.command(help="Display all app BundleId")
 def db():
     app_lists = get_app_list_by_list(app_path)
-    pre_size = len(str(len(app_lists)))
-    app_bundle_id_list = [get_app_bundleid(i) for i in app_lists]
-
-    columns = get_terminal_columns()
-    app_size = get_list_max_size(app_lists)
-    app_bundle_id_size = get_list_max_size(app_bundle_id_list)
-
-    num = columns // (app_size + 4 + pre_size + 2 + app_bundle_id_size + 2)
-    if num == 0:
-        num = 1
-    messages = ""
-    split = "".rjust(2)
-    for i, appname in enumerate(app_lists):
-        numstr = typer.style(f"{i + 1}".rjust(pre_size, "0"), fg=Colors.INFO.value)
-        appname = typer.style(appname.ljust(app_size), fg=Colors.INFO.value)
-        appbundleid = typer.style(
-            app_bundle_id_list[i].ljust(app_bundle_id_size), fg=Colors.INFO.value
-        )
-        message = f"{numstr}: {appname}{split}{appbundleid}{split}{split}"
-        messages = f"{messages}{message}"
-        if (i + 1) % num == 0:
-            typer.echo(messages)
-            messages = ""
+    app_dict = {i: get_app_bundleid(i) for i in app_lists}
+    term_fmt_by_dict(app_dict)
 
 
 @app.command(help="get one app bundleid")
@@ -113,6 +66,7 @@ def get(
         )
         raise typer.Abort()
 
+
 @app.callback(invoke_without_command=True)
 def default(ctx: typer.Context) -> None:
     """Sub-command that I would like to be the default."""
@@ -120,6 +74,7 @@ def default(ctx: typer.Context) -> None:
         # print("Skipping default command to run sub-command.")
         return
     display()
+
 
 if __name__ == "__main__":
     app()
--- sd/utils/fmt.py
+++ sd/utils/fmt.py
@@ -38,17 +38,51 @@ def term_fmt_by_list(lst: List[str], space: int = 4):
     """
     pre_size = str_len(str(len(lst)))
     term_columns = columns()
-    cell_size = max_size(lst)
+    ceil_size = max_size(lst)
     sep_len = str_len(": ")
-    num = term_columns // (pre_size + sep_len + space + cell_size)
+    ceil_size_and_sep = pre_size + sep_len + ceil_size
+    num = term_columns // ceil_size_and_sep
     if num == 0:
         num = 1
+    elif (num - 1) * space + num * ceil_size_and_sep > term_columns:
+        num -= 1
     messages = ""
     split = " " * space
-    for i, cell_msg in enumerate(lst):
+    for i, ceil_msg in enumerate(lst):
         numstr=typer.style(str_rjust(f"{i + 1}", pre_size, "0"), fg=Colors.INFO.value)
-        cell_msg = typer.style(str_ljust(cell_msg, cell_size), fg=Colors.INFO.value)
-        messages += f"{numstr}: {cell_msg}{split}"
+        ceil_msg = typer.style(str_ljust(ceil_msg, ceil_size), fg=Colors.INFO.value)
         if (i + 1) % num == 0:
+            messages += f"{numstr}: {ceil_msg}"
             typer.echo(messages)
             messages = ""
+        else:
+            messages += f"{numstr}: {ceil_msg}{split}"
+
+def term_fmt_by_dict(dic: dict[str, str], space: int = 4, dic_sep: str = "   ") -> None:
+    pre_keys = dic.keys()
+    pre_vals = dic.values()
+    pre_size = str_len(str(len(pre_keys)))
+    term_columns = columns()
+    keys_size = max_size(pre_keys)
+    vals_size = max_size(pre_vals)
+    split = " " * space
+    sep_len = str_len(": ")
+    dic_sep_len = str_len(dic_sep)
+    ceil_len = keys_size + vals_size + sep_len + dic_sep_len + pre_size
+    num = term_columns // ceil_len
+    if num == 0:
+        num = 1
+    elif (num - 1) * space + num * ceil_len > term_columns:
+        num -= 1
+    messages = ""
+    for i, key_msg in enumerate(pre_keys):
+        numstr = typer.style(str_rjust(f"{i + 1}", pre_size, "0"), fg=Colors.INFO.value)
+        ceil_msg = typer.style(str_ljust(key_msg, keys_size), fg=Colors.INFO.value)
+        dic_msg = typer.style(dic_sep, fg=Colors.WARN.value)
+        val_msg = typer.style(str_ljust(dic[key_msg], vals_size), fg=Colors.INFO.value)
+        if (i + 1) % num == 0:
+            messages += f"{numstr}: {ceil_msg}{dic_msg}{val_msg}"
+            typer.echo(messages)
+            messages = ""
+        else:
+            messages += f"{numstr}: {ceil_msg}{dic_msg}{val_msg}{split}"
