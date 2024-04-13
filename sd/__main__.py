c16ade64c16f37a6e4b83c04e55c8dd2db285c6d
Shanyou Li <shanyouli6@gmail.com>
2024-04-13 19:16:56 +0800

    test

--- sd/__main__.py
+++ sd/__main__.py
@@ -1,47 +1,34 @@
 #!/usr/bin/env python3
-import json
-import os
-import re
-import subprocess
-from functools import wraps
-from pathlib import Path
-from typing import List
 
 import typer
 
 from sd.api import env, macbid, macos, nix
 from sd.utils.enums import (
-    ISLINUX,
     ISMAC,
-    SYSTEM_ARCH,
-    SYSTEM_OS,
-    USERNAME,
-    Colors,
-    FlakeOutputs,
 )
 
 app = typer.Typer(add_completion=True)
 app.add_typer(
     macbid.app,
-    name="bid",
-    help="Get macos App BundleID!",
+    name='bid',
+    help='Get macos App BundleID!',
     hidden=not ISMAC,
 )
 app.add_typer(
     env.app,
-    name="env",
-    help="save shell environment variable",
+    name='env',
+    help='save shell environment variable',
 )  #  callback=env_app_callback
 
 app.add_typer(
     macos.app,
-    name="darwin",
-    help="macos Commonly used shortcut commands",
+    name='darwin',
+    help='macos Commonly used shortcut commands',
     hidden=not ISMAC,
 )
 
-app.add_typer(nix.app, name="sys", help="System Configuration Management By Nix")
+app.add_typer(nix.app, name='sys', help='System Configuration Management By Nix')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app()
