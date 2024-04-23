541e545d1436eb8ad56307a57273d13580a0f641
Shanyou Li <shanyouli6@gmail.com>
2024-04-23 18:34:26 +0800

    fix dotfiles env error

--- sd/utils/enums.py
+++ sd/utils/enums.py
@@ -14,37 +14,39 @@ class Colors(Enum):
 
 
 class FlakeOutputs(Enum):
-    NIXOS = "nixosConfigurations"
-    DARWIN = "darwinConfigurations"
-    HOME_MANAGER = "homeConfigurations"
+    NIXOS = 'nixosConfigurations'
+    DARWIN = 'darwinConfigurations'
+    HOME_MANAGER = 'homeConfigurations'
+
 
 class Dotfiles:
     @property
     def value(self):
+        env_dotfiles = os.getenv('DOTFILES') or ''
         for i in [
-            os.getenv("DOTFILES"),
-            "/etc/dotfiles",
-            "/etc/nixos",
-            os.path.expanduser("~/.config/dotfiles"),
-            os.path.expanduser("~/.dotfiles"),
-            os.path.expanduser("~/.nixpkgs"),
+            env_dotfiles,
+            '/etc/dotfiles',
+            '/etc/nixos',
+            os.path.expanduser('~/.config/dotfiles'),
+            os.path.expanduser('~/.dotfiles'),
+            os.path.expanduser('~/.nixpkgs'),
         ]:
             if (
                 os.path.isdir(i)
-                and os.path.exists(os.path.join(i, "flake.nix"))
-                and os.path.exists(os.path.join(i, ".git"))
+                and os.path.exists(os.path.join(i, 'flake.nix'))
+                and os.path.exists(os.path.join(i, '.git'))
             ):
                 return os.path.realpath(i)
 
+
 UNAME = platform.uname()
-user_id = run(["id", "-un"], capture_output=True).stdout.decode().strip()
+user_id = run(['id', '-un'], capture_output=True).stdout.decode().strip()
 
 SYSTEM_OS = UNAME.system.lower()
-SYSTEM_ARCH = "aarch64" if UNAME.machine == "arm64" else UNAME.machine
-USERNAME = os.getenv("USER") if user_id == "root" else user_id
+SYSTEM_ARCH = 'aarch64' if UNAME.machine == 'arm64' else UNAME.machine
+USERNAME = os.getenv('USER') if user_id == 'root' else user_id
 
-ISMAC = SYSTEM_OS == "darwin"
-ISLINUX = SYSTEM_OS == "linux"
+ISMAC = SYSTEM_OS == 'darwin'
+ISLINUX = SYSTEM_OS == 'linux'
 
-REMOTE_FLAKE = "github:shanyouli/dotfiles"
-
+REMOTE_FLAKE = 'github:shanyouli/dotfiles'
